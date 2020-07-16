c3becff013b7895e68c9ec5ca1b00ae220b01059
Eric Stanley <ericstanley@pop-os.localdomain>
2020-07-16 20:52:22 +0530

    Added response for OPTIONS request

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(fileHelper.imageStore.uploadToCloud);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
